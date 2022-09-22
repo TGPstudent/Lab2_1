629c3eed6111ce432d3401380385e3fbfe158232
TGPstudent <tarasenko.student@gmail.com>
2022-09-22 21:49:59 +0300

    Зміна кодування

--- Lab2_1/Lab2_1.c
+++ Lab2_1/Lab2_1.c
@@ -11,23 +11,21 @@ int a, b, rez1, rez2, rez3;
 int main()
 {
 	setlocale(LC_CTYPE, "ukr");
-
 	printf("\n ¬ведiть два цiлих числа: \n");
-	printf("\n \t введiть число a="); 
+	printf("\n \t введiть число a=");
 	scanf("%d", &a);
-	printf("\n \t ¬ведiть вiдмiнне вiд 0 число b="); 
+	printf("\n \t ¬ведiть вiдмiнне вiд 0 число b=");
 	scanf("%d", &b);
 
-	rez1 = a % b; 
-	rez2 = b - a; 
+	rez1 = a % b;
+	rez2 = b - a;
 	//rez3 = (a + b)*(a + b);
 	rez3 = pow((a + b), 2);
 
 	printf("\n \n ќ–»ћјЌ»… –≈«”Ћ№“ј“:");
-	printf("\n \t «алишок вiд дiленн€ %d / %d = %d", a, b, rez1); 
+	printf("\n \t «алишок вiд дiленн€ %d / %d = %d", a, b, rez1);
 	printf("\n \t –iзниц€ %d - %d = %d", b, a, rez2);
 	printf("\n \t —ума чисел в квадратi (%d + %d)^2 = %d", a, b, rez3);
-	
 	int getch(); getch();
 
 	return 0;
